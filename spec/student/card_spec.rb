27a618e1c09574cd342786874db6010b1d701782
Emily Kanarek <emilyakanarek@gmail.com>
2025-09-03 10:54:09 -0400

    Write tests for Ace cards

--- spec/student/card_spec.rb
+++ spec/student/card_spec.rb
@@ -1,5 +1,15 @@
 # frozen_string_literal: true
 
-describe Card do
-  # ...student specs...
+describe(Card) do
+  context("card is an ace") do 
+    subject { Card.new("Hearts", "A") }
+
+    it "has a rank of 'A'" do 
+      expect(subject.ace?).to be(true)
+    end
+
+    it "has a value of 11" do 
+      expect(subject.value).to be(11) 
+    end
+  end
 end
