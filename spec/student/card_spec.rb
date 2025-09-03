bef0c3eabb3df283508252f41d157972f4925171
Mike <mclancy96@gmail.com>
2025-09-03 11:59:06 -0400

    Water it down

--- spec/student/card_spec.rb
+++ spec/student/card_spec.rb
@@ -1,5 +1,257 @@
 # frozen_string_literal: true
 
 describe Card do
-  # ...student specs...
+  it 'creates a card with valid suit and rank' do
+    card = Card.new('Hearts', 'A')
+    expect(card.suit).to eq('Hearts')
+    expect(card.rank).to eq('A')
+  end
+
+  it 'creates another card with valid suit and rank' do
+    card = Card.new('Diamonds', 'K')
+    expect(card.suit).to eq('Diamonds')
+    expect(card.rank).to eq('K')
+  end
+
+  it 'creates a third card with valid suit and rank' do
+    card = Card.new('Clubs', '10')
+    expect(card.suit).to eq('Clubs')
+    expect(card.rank).to eq('10')
+  end
+
+  it 'raises error for invalid suit' do
+    expect { Card.new('InvalidSuit', 'A') }.to raise_error(ArgumentError, 'Invalid suit')
+  end
+
+  it 'raises error for another invalid suit' do
+    expect { Card.new('NotASuit', 'K') }.to raise_error(ArgumentError, 'Invalid suit')
+  end
+
+  it 'raises error for invalid rank' do
+    expect { Card.new('Hearts', 'InvalidRank') }.to raise_error(ArgumentError, 'Invalid rank')
+  end
+
+  it 'raises error for another invalid rank' do
+    expect { Card.new('Spades', 'NotARank') }.to raise_error(ArgumentError, 'Invalid rank')
+  end
+
+  it 'identifies jack as face card' do
+    card = Card.new('Hearts', 'J')
+    expect(card.face_card?).to be true
+  end
+
+  it 'identifies queen as face card' do
+    card = Card.new('Diamonds', 'Q')
+    expect(card.face_card?).to be true
+  end
+
+  it 'identifies king as face card' do
+    card = Card.new('Clubs', 'K')
+    expect(card.face_card?).to be true
+  end
+
+  it 'identifies jack of spades as face card' do
+    card = Card.new('Spades', 'J')
+    expect(card.face_card?).to be true
+  end
+
+  it 'identifies queen of hearts as face card' do
+    card = Card.new('Hearts', 'Q')
+    expect(card.face_card?).to be true
+  end
+
+  it 'identifies king of diamonds as face card' do
+    card = Card.new('Diamonds', 'K')
+    expect(card.face_card?).to be true
+  end
+
+  it 'identifies ace as not a face card' do
+    card = Card.new('Hearts', 'A')
+    expect(card.face_card?).to be false
+  end
+
+  it 'identifies 2 as not a face card' do
+    card = Card.new('Diamonds', '2')
+    expect(card.face_card?).to be false
+  end
+
+  it 'identifies 10 as not a face card' do
+    card = Card.new('Clubs', '10')
+    expect(card.face_card?).to be false
+  end
+
+  it 'identifies ace of hearts as ace' do
+    card = Card.new('Hearts', 'A')
+    expect(card.ace?).to be true
+  end
+
+  it 'identifies ace of diamonds as ace' do
+    card = Card.new('Diamonds', 'A')
+    expect(card.ace?).to be true
+  end
+
+  it 'identifies ace of clubs as ace' do
+    card = Card.new('Clubs', 'A')
+    expect(card.ace?).to be true
+  end
+
+  it 'identifies ace of spades as ace' do
+    card = Card.new('Spades', 'A')
+    expect(card.ace?).to be true
+  end
+
+  it 'identifies king as not an ace' do
+    card = Card.new('Hearts', 'K')
+    expect(card.ace?).to be false
+  end
+
+  it 'identifies queen as not an ace' do
+    card = Card.new('Diamonds', 'Q')
+    expect(card.ace?).to be false
+  end
+
+  it 'identifies jack as not an ace' do
+    card = Card.new('Clubs', 'J')
+    expect(card.ace?).to be false
+  end
+
+  it 'identifies 2 as not an ace' do
+    card = Card.new('Spades', '2')
+    expect(card.ace?).to be false
+  end
+
+  it 'calculates ace value as 11' do
+    card = Card.new('Hearts', 'A')
+    expect(card.value).to eq(11)
+  end
+
+  it 'calculates ace of diamonds value as 11' do
+    card = Card.new('Diamonds', 'A')
+    expect(card.value).to eq(11)
+  end
+
+  it 'calculates ace of clubs value as 11' do
+    card = Card.new('Clubs', 'A')
+    expect(card.value).to eq(11)
+  end
+
+  it 'calculates ace of spades value as 11' do
+    card = Card.new('Spades', 'A')
+    expect(card.value).to eq(11)
+  end
+
+  it 'calculates jack value as 10' do
+    card = Card.new('Hearts', 'J')
+    expect(card.value).to eq(10)
+  end
+
+  it 'calculates queen value as 10' do
+    card = Card.new('Diamonds', 'Q')
+    expect(card.value).to eq(10)
+  end
+
+  it 'calculates king value as 10' do
+    card = Card.new('Clubs', 'K')
+    expect(card.value).to eq(10)
+  end
+
+  it 'calculates jack of spades value as 10' do
+    card = Card.new('Spades', 'J')
+    expect(card.value).to eq(10)
+  end
+
+  it 'calculates queen of hearts value as 10' do
+    card = Card.new('Hearts', 'Q')
+    expect(card.value).to eq(10)
+  end
+
+  it 'calculates king of diamonds value as 10' do
+    card = Card.new('Diamonds', 'K')
+    expect(card.value).to eq(10)
+  end
+
+  it 'calculates 2 value as 2' do
+    card = Card.new('Hearts', '2')
+    expect(card.value).to eq(2)
+  end
+
+  it 'calculates 3 value as 3' do
+    card = Card.new('Diamonds', '3')
+    expect(card.value).to eq(3)
+  end
+
+  it 'calculates 4 value as 4' do
+    card = Card.new('Clubs', '4')
+    expect(card.value).to eq(4)
+  end
+
+  it 'calculates 5 value as 5' do
+    card = Card.new('Spades', '5')
+    expect(card.value).to eq(5)
+  end
+
+  it 'calculates 6 value as 6' do
+    card = Card.new('Hearts', '6')
+    expect(card.value).to eq(6)
+  end
+
+  it 'calculates 7 value as 7' do
+    card = Card.new('Diamonds', '7')
+    expect(card.value).to eq(7)
+  end
+
+  it 'calculates 8 value as 8' do
+    card = Card.new('Clubs', '8')
+    expect(card.value).to eq(8)
+  end
+
+  it 'calculates 9 value as 9' do
+    card = Card.new('Spades', '9')
+    expect(card.value).to eq(9)
+  end
+
+  it 'calculates 10 value as 10' do
+    card = Card.new('Hearts', '10')
+    expect(card.value).to eq(10)
+  end
+
+  it 'formats ace of hearts string correctly' do
+    card = Card.new('Hearts', 'A')
+    expect(card.to_s).to eq('A of Hearts')
+  end
+
+  it 'formats king of diamonds string correctly' do
+    card = Card.new('Diamonds', 'K')
+    expect(card.to_s).to eq('K of Diamonds')
+  end
+
+  it 'formats queen of clubs string correctly' do
+    card = Card.new('Clubs', 'Q')
+    expect(card.to_s).to eq('Q of Clubs')
+  end
+
+  it 'formats jack of spades string correctly' do
+    card = Card.new('Spades', 'J')
+    expect(card.to_s).to eq('J of Spades')
+  end
+
+  it 'formats 10 of hearts string correctly' do
+    card = Card.new('Hearts', '10')
+    expect(card.to_s).to eq('10 of Hearts')
+  end
+
+  it 'formats 2 of diamonds string correctly' do
+    card = Card.new('Diamonds', '2')
+    expect(card.to_s).to eq('2 of Diamonds')
+  end
+
+  it 'formats 3 of clubs string correctly' do
+    card = Card.new('Clubs', '3')
+    expect(card.to_s).to eq('3 of Clubs')
+  end
+
+  it 'formats 9 of spades string correctly' do
+    card = Card.new('Spades', '9')
+    expect(card.to_s).to eq('9 of Spades')
+  end
 end
--- spec/student/deck_spec.rb
+++ spec/student/deck_spec.rb
@@ -1,5 +1,313 @@
 # frozen_string_literal: true
 
 describe Deck do
-  # ...student specs...
+  it 'creates a deck with 52 cards' do
+    deck = Deck.new
+    expect(deck.size).to eq(52)
+  end
+
+  it 'creates another deck with 52 cards' do
+    deck = Deck.new
+    expect(deck.size).to eq(52)
+  end
+
+  it 'creates a full deck that is not empty' do
+    deck = Deck.new
+    expect(deck.empty?).to be false
+  end
+
+  it 'creates another full deck that is not empty' do
+    deck = Deck.new
+    expect(deck.empty?).to be false
+  end
+
+  it 'has all four suits in new deck' do
+    deck = Deck.new
+    suits = deck.cards.map(&:suit).uniq.sort
+    expect(suits).to eq(['Clubs', 'Diamonds', 'Hearts', 'Spades'])
+  end
+
+  it 'verifies all four suits exist in another new deck' do
+    deck = Deck.new
+    suits = deck.cards.map(&:suit).uniq.sort
+    expect(suits).to eq(['Clubs', 'Diamonds', 'Hearts', 'Spades'])
+  end
+
+  it 'has all thirteen ranks in new deck' do
+    deck = Deck.new
+    ranks = deck.cards.map(&:rank).uniq.sort_by { |rank| Card::RANKS.index(rank) }
+    expect(ranks).to eq(['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'])
+  end
+
+  it 'verifies all thirteen ranks exist in another new deck' do
+    deck = Deck.new
+    ranks = deck.cards.map(&:rank).uniq.sort_by { |rank| Card::RANKS.index(rank) }
+    expect(ranks).to eq(['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'])
+  end
+
+  it 'can draw a card from the deck' do
+    deck = Deck.new
+    card = deck.draw
+    expect(card).to be_a(Card)
+  end
+
+  it 'can draw another card from the deck' do
+    deck = Deck.new
+    card = deck.draw
+    expect(card).to be_a(Card)
+  end
+
+  it 'reduces deck size by 1 after drawing' do
+    deck = Deck.new
+    original_size = deck.size
+    deck.draw
+    expect(deck.size).to eq(original_size - 1)
+  end
+
+  it 'reduces deck size correctly after drawing a card' do
+    deck = Deck.new
+    original_size = deck.size
+    deck.draw
+    expect(deck.size).to eq(original_size - 1)
+  end
+
+  it 'reduces deck size by 2 after drawing twice' do
+    deck = Deck.new
+    original_size = deck.size
+    deck.draw
+    deck.draw
+    expect(deck.size).to eq(original_size - 2)
+  end
+
+  it 'properly decreases size after drawing multiple cards' do
+    deck = Deck.new
+    original_size = deck.size
+    deck.draw
+    deck.draw
+    expect(deck.size).to eq(original_size - 2)
+  end
+
+  it 'can draw 3 cards and reduce size by 3' do
+    deck = Deck.new
+    original_size = deck.size
+    deck.draw
+    deck.draw
+    deck.draw
+    expect(deck.size).to eq(original_size - 3)
+  end
+
+  it 'correctly handles drawing 3 cards' do
+    deck = Deck.new
+    original_size = deck.size
+    deck.draw
+    deck.draw
+    deck.draw
+    expect(deck.size).to eq(original_size - 3)
+  end
+
+  it 'can draw 5 cards and reduce size by 5' do
+    deck = Deck.new
+    original_size = deck.size
+    5.times { deck.draw }
+    expect(deck.size).to eq(original_size - 5)
+  end
+
+  it 'properly handles drawing 5 cards' do
+    deck = Deck.new
+    original_size = deck.size
+    5.times { deck.draw }
+    expect(deck.size).to eq(original_size - 5)
+  end
+
+  it 'can draw 10 cards and reduce size by 10' do
+    deck = Deck.new
+    original_size = deck.size
+    10.times { deck.draw }
+    expect(deck.size).to eq(original_size - 10)
+  end
+
+  it 'correctly manages drawing 10 cards' do
+    deck = Deck.new
+    original_size = deck.size
+    10.times { deck.draw }
+    expect(deck.size).to eq(original_size - 10)
+  end
+
+  it 'becomes empty after drawing all cards' do
+    deck = Deck.new
+    52.times { deck.draw }
+    expect(deck.empty?).to be true
+  end
+
+  it 'is empty after drawing all 52 cards' do
+    deck = Deck.new
+    52.times { deck.draw }
+    expect(deck.empty?).to be true
+  end
+
+  it 'has size 0 when empty' do
+    deck = Deck.new
+    52.times { deck.draw }
+    expect(deck.size).to eq(0)
+  end
+
+  it 'reports size as 0 when all cards drawn' do
+    deck = Deck.new
+    52.times { deck.draw }
+    expect(deck.size).to eq(0)
+  end
+
+  it 'returns nil when drawing from empty deck' do
+    deck = Deck.new
+    52.times { deck.draw }
+    expect(deck.draw).to be_nil
+  end
+
+  it 'gives nil when trying to draw from empty deck' do
+    deck = Deck.new
+    52.times { deck.draw }
+    expect(deck.draw).to be_nil
+  end
+
+  it 'can shuffle the deck' do
+    deck = Deck.new
+    original_order = deck.cards.dup
+    deck.shuffle!
+    # Very unlikely to be in same order after shuffle
+    expect(deck.cards).not_to eq(original_order)
+  end
+
+  it 'shuffles and maintains deck size' do
+    deck = Deck.new
+    original_size = deck.size
+    deck.shuffle!
+    expect(deck.size).to eq(original_size)
+  end
+
+  it 'keeps all cards after shuffling' do
+    deck = Deck.new
+    original_cards = deck.cards.dup
+    deck.shuffle!
+    expect(deck.cards.sort_by(&:to_s)).to eq(original_cards.sort_by(&:to_s))
+  end
+
+  it 'maintains all cards when shuffled' do
+    deck = Deck.new
+    original_cards = deck.cards.dup
+    deck.shuffle!
+    expect(deck.cards.sort_by(&:to_s)).to eq(original_cards.sort_by(&:to_s))
+  end
+
+  it 'can draw from shuffled deck' do
+    deck = Deck.new
+    deck.shuffle!
+    card = deck.draw
+    expect(card).to be_a(Card)
+  end
+
+  it 'draws cards properly from shuffled deck' do
+    deck = Deck.new
+    deck.shuffle!
+    card = deck.draw
+    expect(card).to be_a(Card)
+  end
+
+  it 'shuffled deck still reduces size when drawing' do
+    deck = Deck.new
+    deck.shuffle!
+    original_size = deck.size
+    deck.draw
+    expect(deck.size).to eq(original_size - 1)
+  end
+
+  it 'shuffled deck decreases size correctly' do
+    deck = Deck.new
+    deck.shuffle!
+    original_size = deck.size
+    deck.draw
+    expect(deck.size).to eq(original_size - 1)
+  end
+
+  it 'can shuffle multiple times' do
+    deck = Deck.new
+    deck.shuffle!
+    deck.shuffle!
+    deck.shuffle!
+    expect(deck.size).to eq(52)
+  end
+
+  it 'maintains size after multiple shuffles' do
+    deck = Deck.new
+    deck.shuffle!
+    deck.shuffle!
+    deck.shuffle!
+    expect(deck.size).to eq(52)
+  end
+
+  it 'can be completely emptied by drawing' do
+    deck = Deck.new
+    cards_drawn = []
+    while !deck.empty?
+      cards_drawn << deck.draw
+    end
+    expect(cards_drawn.length).to eq(52)
+  end
+
+  it 'draws exactly 52 unique cards' do
+    deck = Deck.new
+    cards_drawn = []
+    while !deck.empty?
+      cards_drawn << deck.draw
+    end
+    expect(cards_drawn.length).to eq(52)
+    expect(cards_drawn.uniq.length).to eq(52)
+  end
+
+  it 'has hearts cards in full deck' do
+    deck = Deck.new
+    hearts_cards = deck.cards.select { |card| card.suit == 'Hearts' }
+    expect(hearts_cards.length).to eq(13)
+  end
+
+  it 'contains 13 hearts cards' do
+    deck = Deck.new
+    hearts_cards = deck.cards.select { |card| card.suit == 'Hearts' }
+    expect(hearts_cards.length).to eq(13)
+  end
+
+  it 'has diamonds cards in full deck' do
+    deck = Deck.new
+    diamonds_cards = deck.cards.select { |card| card.suit == 'Diamonds' }
+    expect(diamonds_cards.length).to eq(13)
+  end
+
+  it 'contains 13 diamonds cards' do
+    deck = Deck.new
+    diamonds_cards = deck.cards.select { |card| card.suit == 'Diamonds' }
+    expect(diamonds_cards.length).to eq(13)
+  end
+
+  it 'has clubs cards in full deck' do
+    deck = Deck.new
+    clubs_cards = deck.cards.select { |card| card.suit == 'Clubs' }
+    expect(clubs_cards.length).to eq(13)
+  end
+
+  it 'contains 13 clubs cards' do
+    deck = Deck.new
+    clubs_cards = deck.cards.select { |card| card.suit == 'Clubs' }
+    expect(clubs_cards.length).to eq(13)
+  end
+
+  it 'has spades cards in full deck' do
+    deck = Deck.new
+    spades_cards = deck.cards.select { |card| card.suit == 'Spades' }
+    expect(spades_cards.length).to eq(13)
+  end
+
+  it 'contains 13 spades cards' do
+    deck = Deck.new
+    spades_cards = deck.cards.select { |card| card.suit == 'Spades' }
+    expect(spades_cards.length).to eq(13)
+  end
 end
