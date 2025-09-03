62dc62ed5efc68da75c9df34add2b49b1d1a5a70
Emily Kanarek <emilyakanarek@gmail.com>
2025-09-03 11:07:41 -0400

    Change syntax to pass tests

--- spec/student/deck_spec.rb
+++ spec/student/deck_spec.rb
@@ -1,11 +1,11 @@
 # frozen_string_literal: true
 
 describe(Deck) do
-  subject(:deck) { Deck.new } 
+  subject { Deck.new } 
 
   context("when initialized") do 
     it "has 52 cards" do 
-      expect(deck.size).to eq(52) 
+      expect(subject.size).to eq(52) 
     end
   end
 end
