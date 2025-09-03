7625c504343703eeac7a2e35da8fc320e1217239
Emily Kanarek <emilyakanarek@gmail.com>
2025-09-03 11:03:55 -0400

    Check if deck has 52 cards

--- spec/student/deck_spec.rb
+++ spec/student/deck_spec.rb
@@ -1,5 +1,11 @@
 # frozen_string_literal: true
 
-describe Deck do
-  # ...student specs...
+describe(Deck) do
+  subject(:deck) { Deck.new } 
+
+  context("when initialized") do 
+    it "has 52 cards" do 
+      expect(deck.size).to eq(52) 
+    end
+  end
 end
