32d55de0de37994e129f34a6cb0938849861b9db
Kyle <kylegranthousel@gmail.com>
2025-09-03 11:28:18 -0400

    Add shared examples

--- spec/student/card_spec.rb
+++ spec/student/card_spec.rb
@@ -1,26 +1,36 @@
-# frozen_string_literal: true
-
 describe(Card) do
   let(:card) { Card.new("Hearts", "7") }
 
-  describe("#initialize") do
-    context("when initialized with valid attributes") do
-      it "creates a card" do
+  describe "#initialize" do
+    context "when initialized with valid attributes" do
+      it 'has the correct suit' do
         expect(card.suit).to eq("Hearts")
+      end
+
+      it 'has the correct rank' do
         expect(card.rank).to eq("7")
       end
     end
 
-    context("when initialized with invalid attributes") do
-      it "raises an error if suit is invalid" do
-        expect { Card.new("Harts", "K") }
-          .to raise_error(ArgumentError, /Invalid suit/)
-      end
+    context "with invalid suit" do
+      let(:build_call) { -> { Card.new("Harts", "K") } }
+      it_behaves_like "rejects invalid input"
+    end
 
-      it "raises an error if rank is invalid" do
-        expect { Card.new("Diamonds", "king") }
-          .to raise_error(ArgumentError, /Invalid rank/)
-      end
+    context "with invalid rank" do
+      let(:build_call) { -> { Card.new("Diamonds", "king") } }
+      it_behaves_like "rejects invalid input"
+    end
+  end
+
+  describe "#ace?" do
+    it 'returns true for A' do
+      ace = Card.new("Hearts", "A")
+      expect(ace.ace?).to eq(true)
+    end
+
+    it 'returns false for non-A' do
+      expect(card.ace?).to eq(false)
     end
   end
 end
